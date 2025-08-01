2032c31a51ea5704b3b061370467067c849590ff
brunaseifert <brunaseifer@gmail.com>
2025-08-01 10:46:54 -0300

    Update main.py

--- Multiplication Table/main.py
+++ Multiplication Table/main.py
@@ -1,3 +1,6 @@
+# Multiplication Table
+# This program prompts the user to enter two numbers and displays the multiplication table for each.
+
 # Tabuada de multiplicação
 # Este programa solicita ao usuário dois números e exibe a tabuada de multiplicação para ambos
 
